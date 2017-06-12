cc515498c8e905460615a0cd53a82426e3fdadb7
odindarkester <odin.dark@gmail.com>
2017-06-12 00:49:21 -0500

    WIP adding timer

--- renderer.js
+++ renderer.js
@@ -6,11 +6,32 @@ var btn = document.getElementById('btnToggle');
 //window.alert(btn);
 btn.addEventListener('click', () => {
     //window.alert('click!');
-    var msg = document.createElement('div');
-    msg.style = "border: 1px solid black;";
-    msg.innerHTML = "Some message here";
-    document.body.appendChild(msg);
+
+    // var Stopwatch = require("node-stopwatch").Stopwatch;
+    // var stopwatch = Stopwatch.create();
+    // stopwatch.start();
     
+    // console.log("ticks: " + stopwatch.elapsedTicks);
+    // console.log("milliseconds: " + stopwatch.elapsedMilliseconds);
+    // console.log("seconds: " + stopwatch.elapsed.seconds);
+    // console.log("minutes: " + stopwatch.elapsed.minutes);
+    // console.log("hours: " + stopwatch.elapsed.hours);
+    
+    //stop it now 
+    //stopwatch.stop();
+    var seconds = 0;
+    setInterval(function(){
+        // let msg = document.createElement('div');
+        // msg.style = "border: 1px solid black;";
+        // msg.innerHTML = "Seconds: " + seconds;
+        // document.body.appendChild(msg);
+        let div = document.getElementById('divElapsed');
+        div.innerHTML = seconds + " seconds";
+        seconds++;
+    }, 1000);
+
+
+
 
 }, false);
 
