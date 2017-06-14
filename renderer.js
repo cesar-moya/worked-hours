2cc574e8ecd8ecb40b0ef7129f75b872ad6c32a7
odindarkester <odin.dark@gmail.com>
2017-06-14 00:53:13 -0500

    wip

--- renderer.js
+++ renderer.js
@@ -1,35 +1,35 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-var btn = document.getElementById('btnToggle');
-//window.alert(btn);
-btn.addEventListener('click', () => {
-    //window.alert('click!');
+//var seconds = 0;
+var startDate;
+var endDate;
+var elapsed;
+var action = 0; // 0 = Nothing, 1 = Coding, 2 = Meeting, 3 = Other
+var btnCoding = document.getElementById('btnCoding');
 
-    // var Stopwatch = require("node-stopwatch").Stopwatch;
-    // var stopwatch = Stopwatch.create();
-    // stopwatch.start();
+btnCoding.addEventListener('click', () => {
+    if(action == 0){
+        action = 1;
+        btnCoding.innerHTML = "STOP Coding";
+        startDate = Math.floor(Date.now() / 1000);
+    }else{
+        action = 0;
+        btnCoding.innerHTML = "Start Coding";
+    }
     
-    // console.log("ticks: " + stopwatch.elapsedTicks);
-    // console.log("milliseconds: " + stopwatch.elapsedMilliseconds);
-    // console.log("seconds: " + stopwatch.elapsed.seconds);
-    // console.log("minutes: " + stopwatch.elapsed.minutes);
-    // console.log("hours: " + stopwatch.elapsed.hours);
-    
-    //stop it now 
-    //stopwatch.stop();
-    var seconds = 0;
+
     setInterval(function(){
         // let msg = document.createElement('div');
         // msg.style = "border: 1px solid black;";
         // msg.innerHTML = "Seconds: " + seconds;
         // document.body.appendChild(msg);
         let div = document.getElementById('divElapsed');
-        div.innerHTML = seconds + " seconds";
-        seconds++;
+        endDate = Math.floor(Date.now() / 1000);
+        elapsed = endDate - startDate;
+        
+        div.innerHTML = "Elapsed: " + elapsed;
+        
     }, 1000);
-
 
 
 
