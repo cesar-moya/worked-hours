98b8cbb07c11fb7465fe5ad0617ec076382e2f7f
odindarkester <odin.dark@gmail.com>
2017-06-17 18:12:50 -0500

    WIP

--- renderer.js
+++ renderer.js
@@ -15,9 +15,9 @@ btnCoding.addEventListener('click', () => {
     }else{
         action = 0;
         btnCoding.innerHTML = "Start Coding";
+        //save to file or embedded db?
     }
     
-
     setInterval(function(){
         // let msg = document.createElement('div');
         // msg.style = "border: 1px solid black;";
@@ -30,10 +30,51 @@ btnCoding.addEventListener('click', () => {
         div.innerHTML = "Elapsed: " + elapsed;
         
     }, 1000);
-
-
-
 }, false);
+
+var btnTest = document.getElementById('btnTest');
+var dBName = 'workDb';
+var dBVersion = 2;
+
+btnTest.addEventListener('click', () => {
+    if(window.indexedDB){
+        //window.alert('indexDB ok');
+        //using HTML5 IndexedDB: https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API/Using_IndexedDB
+        var request = window.indexedDB.open(dBName, dBVersion);
+        var db;
+        request.onerror = function(event){
+            //TODO: handle VER_ERR, catch the upgrade needed error when upgrading the version
+            window.alert('There was an error while initializing the internal database, please allow me to create it!, Error Code: ' + event.target.errorCode);
+        };
+        request.onupgradeneeded = function(event){
+            alert('upgrade needed');
+            db = event.target.result;
+            var objectStore = db.createObjectStore('worklog', { keyPath : 'id' } );
+            //work: {id, workType, startDate, endDate, elapsedSeconds}
+            //objectStore.createIndex()
+            objectStore.transaction.oncomplete = function(event){
+                //todo
+            };
+        };
+        request.onsuccess = function(event){
+            db = event.target.result; // an instance of IDBDatabase
+            window.alert('success');
+        };
+
+    }else{
+        window.alert('indexedDB not supported');
+    }
+    
+}, false);
+
+
+
+
+
+
+
+
+
 
 
 
